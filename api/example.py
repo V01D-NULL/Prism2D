f63a901d4d33be3a32720437e50f3c1e78b134ac
V01D <58613685+V01D-NULL@users.noreply.github.com>
2021-06-19 21:53:13 +0200

    Create example.py

---
+++ api/example.py
@@ -0,0 +1,17 @@
+from prism2d.Prism2d import Console, Renderer
+
+# Called before the first frame is executed
+def Enter():
+        Console.log("Hello engine")
+
+# Called once per frame, use Update to modify the position of game objects
+def Update():
+        pass
+
+# Called once per frame, used to update a position after update (if required)
+def LateUpdate():
+        pass
+
+# Called once per frame, use only for rendering
+def RenderCycle():
+        Renderer.clear(102, 153, 255) #Cornflower blue
